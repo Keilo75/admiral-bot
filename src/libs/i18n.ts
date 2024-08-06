4126ffd2ebf16192d53e5436e5719b1580a2d464
Keilo75 <82605039+Keilo75@users.noreply.github.com.>
2024-08-06 22:02:56 +0000

    feat: setup i18n

---
+++ src/libs/i18n.ts
@@ -0,0 +1,12 @@
+import i18next from "i18next";
+
+import en from "../assets/en.json";
+
+i18next.init({
+  lng: "en",
+  resources: {
+    en: {
+      translation: en,
+    },
+  },
+});
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import "./libs/i18n";
+
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
+import i18next from "i18next";
 
 import { commands } from "./commands";
 import { Logger } from "./utils";
@@ -8,6 +11,7 @@ dotenv.config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+Logger.info("Logging in...");
 client.once(Events.ClientReady, (readyClient) => {
   Logger.info(`Logged in as ${readyClient.user.tag}.`);
 });
@@ -20,14 +24,22 @@ client.on(Events.InteractionCreate, async (interaction) => {
   if (!command) {
     // TODO: Display error to user
     Logger.error(`Unable to find '${commandName}' command.`);
+    await interaction.reply(i18next.t("system.error"));
     return;
   }
 
   try {
     await command.execute(interaction);
   } catch (err) {
-    // TODO: Display error to user.
     Logger.error(`Error while executing ${commandName}.`, err);
+
+    const content = i18next.t("system.error");
+
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content });
+    } else {
+      await interaction.reply({ content });
+    }
   }
 });
 
